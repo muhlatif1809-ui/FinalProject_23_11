f3629b74c17e3aeb3f3bf72b917f3aa9f1eeeb67
firlandofarel27 <firlandofarel27@gmail.com>
2025-10-02 15:14:18 +0700

    sesss

--- Test.java
+++ Test.java
@@ -1,6 +1,8 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hi ini baris kode petama");
+        System.out.println("Farel Firlando");
+        
     }
     
 }
