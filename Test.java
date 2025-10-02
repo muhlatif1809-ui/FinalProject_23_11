f0c48ddebd5eb81952d0276a2216a16bfa8e6b3a
muhlatif1809 <muhlatif1809@gmail.com>
2025-10-02 15:14:21 +0700

    1

--- Test.java
+++ Test.java
@@ -1,6 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hi ini baris kode petama");
+        System.out.println("Muhammad Latif");
     }
     
 }
