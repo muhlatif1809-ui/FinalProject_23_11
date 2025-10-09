db901eb4367a88b77d54c128921ce3cf4601f3d1
muhlatif1809 <muhlatif1809@gmail.com>
2025-10-09 09:10:12 +0700

    P6_Percobaam1

---
+++ Latihan.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Latihan {
+    public static void main(String[] args) {
+        Scanner latif = new Scanner(System.in);
+        int nilai = 80;
+        if (nilai > 80) {
+            nilai = latif.nextInt();
+            System.out.println("Lulus");
+            
+        }
+      
